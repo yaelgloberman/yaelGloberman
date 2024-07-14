a193a811732c92934361c755927f40fabd923a6f
Yael <yaelgloiberman@gmail.com>
2024-07-14 14:51:52 +0300

    diffrent changes

--- classroom-management/src/App.js
+++ classroom-management/src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import MenuAppBar from "./components/MenuAppBar";
 
-function App() {
+import React from "react";
+import { BrowserRouter as Router } from "react-router-dom";
+import { Grid, ThemeProvider, createTheme } from "@mui/material";
+import { QueryClient, QueryClientProvider, useQuery } from "react-query";
+import Routes from "./routes";
+import { useAppSelector } from "./redux/store";
+
+const queryClient = new QueryClient();
+
+const App = () => {
+  const darkMode = useAppSelector((state) => state.theme.darkMode);
+  const theme = createTheme({
+    palette: {
+      mode: darkMode ? "dark" : "light",
+    },
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider theme={theme}>
+        <Router>
+          <MenuAppBar />
+            <Routes />
+        </Router>
+      </ThemeProvider>
+    </QueryClientProvider>
   );
-}
+};
 
 export default App;
