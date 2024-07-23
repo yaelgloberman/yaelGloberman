219b0c85a2a7ea5c705b15bf506cf0a726c45807
Yael <yaelgloiberman@gmail.com>
2024-07-23 15:52:47 +0300

    added delete function

--- classroom-management/src/components/DialogStudent.js
+++ classroom-management/src/components/DialogStudent.js
@@ -11,46 +11,55 @@ import {
   ListItemIcon,
   ListItemText,
 } from "@mui/material";
-import SchoolIcon from "@mui/icons-material/School";
-import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
 import React from "react";
 import Delete from "@mui/icons-material/Delete";
-const DialogStudent = ({ data, handleClose, handleOpen, open }) => {
-
+import { deleteStudentFromClass } from "../services/classService";
+const DialogStudent = ({ setData, data, handleClose, open, onClassUpdated}) => {
   const [selectedIndex, setSelectedIndex] = React.useState(1);
 
   const handleListItemClick = (event, index) => {
     setSelectedIndex(index);
   };
+  const handleDeleteStudent = (student) => {
+    deleteStudentFromClass(student.assignToClass, student.id);
+    const updatedStudents = data.filter((student1) => student1.id !== student.id);
+    setData(updatedStudents);
+    onClassUpdated();
+  };
+
   return (
     <div>
-        <Grid container alignItems="center" justifyContent="center">
+      <Grid container alignItems="center" justifyContent="center">
         <Dialog open={open} onClose={handleClose}>
-      <DialogTitle>Class students </DialogTitle>
-      <DialogContent>
-        <Box sx={{ width: "100%", maxWidth: 300, bgcolor: "background.paper" }}>
-          <List component="nav" aria-label="main mailbox folders">
-            {data?.map((student, index) => (
-              <ListItemButton
-                key={student.id}
-                selected={selectedIndex === index}
-                onClick={(event) => handleListItemClick(event, index)}
-              >
-                <ListItemIcon>
-                  <AccountCircleIcon />
-                </ListItemIcon>
-                <ListItemText primary={student.firstName} />
-                <ListItemIcon>
-                  <Delete />
-                </ListItemIcon>
-              </ListItemButton>
-            ))}
-          </List>
-          <Divider />
-        </Box>
-      </DialogContent>
-      </Dialog>
-      </Grid>;
+          <DialogTitle>Class students </DialogTitle>
+          <DialogContent>
+            <Box
+              sx={{ width: "100%", maxWidth: 300, bgcolor: "background.paper" }}
+            >
+              <List component="nav" aria-label="main mailbox folders">
+                {data?.map((student, index) => (
+                  <ListItemButton
+                    key={student.id}
+                    selected={selectedIndex === index}
+                    onClick={(event) => handleListItemClick(event, index)}
+                  >
+                    <ListItemIcon>
+                      <AccountCircleIcon />
+                    </ListItemIcon>
+                    <ListItemText primary={student.firstName} />
+                    <ListItemIcon>
+                      <Delete onClick={() => handleDeleteStudent(student)} />
+                    </ListItemIcon>
+                  </ListItemButton>
+                ))}
+              </List>
+              <Divider />
+            </Box>
+          </DialogContent>
+        </Dialog>
+      </Grid>
+      ;
     </div>
   );
 };
