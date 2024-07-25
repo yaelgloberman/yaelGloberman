cad644f4cff8fcc0237704f94ec81b3b92f856f9
Yael <yaelgloiberman@gmail.com>
2024-07-25 15:02:41 +0300

    added the component

--- classroom-management/src/components/Classes/Classes/Classes.style.js
+++ classroom-management/src/components/Classes/Classes/Classes.style.js
@@ -5,30 +5,6 @@ export const useStyles = makeStyles(() => ({
     ...(hasStudents && { cursor: "pointer" }),
   }),
 
-  header: ({ isCenter, sourcePage }) => ({
-    fontSize: 18,
-    fontWeight: 600,
-    paddingTop: 10,
- 
-    fontStyle: "normal",
-    alignItems: "center",
- 
-    ...(isCenter
-      ? { display: "grid", gridTemplateColumns: "38px auto 38px" }
-      : { display: "flex", justifyContent: "space-between" }),
- 
-    ...(sourcePage && { paddingBottom: 25 }),
- 
-    "& p": {
-      margin: 0,
-      width: "fit-content"
-    },
- 
-    "& button": {
-      minWidth: "fit-content"
-    }
-  }),
-
 
 }));
 
---
+++ classroom-management/src/components/Classes/OneClass/OneClass.style.js
@@ -0,0 +1,10 @@
+import { makeStyles } from "@mui/styles";
+
+export const useStyles = makeStyles(() => ({
+  title: () => ({
+     cursor: "pointer" ,
+  }),
+
+
+}));
+
