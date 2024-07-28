e49fae7a9bfbb168fc82f3eb22c7a09f5133375c
Yael <yaelgloiberman@gmail.com>
2024-07-28 10:04:51 +0300

    add font rubik

--- classroom-management/src/index.js
+++ classroom-management/src/index.js
@@ -3,16 +3,28 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { Provider } from "react-redux";
 import store from "./redux/store";
-import "./index.css"
 import { QueryClient, QueryClientProvider } from "react-query";
+import theme from "./theme"; // Import your theme
+import { ThemeProvider } from "@mui/material/styles"; // Import ThemeProvider
+import "./index.css"; // Ensure global styles are imported
+import WebFont from "webfontloader";
+ 
 const queryClient = new QueryClient();
+
+WebFont.load({
+  google: {
+    families: ["Rubik:300,400,500,600,700"]
+  }
+});
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
       <QueryClientProvider client={queryClient}>
-        <App />
+        <ThemeProvider theme={theme}>
+          <App />
+        </ThemeProvider>
       </QueryClientProvider>
     </Provider>
   </React.StrictMode>
